06845057c6e195c5fb650780a41ffc73b747c07f
ssoyeeon <ssuu2039@gmail.com>
2025-09-15 10:01:18 +0900

    [feat] 백업

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node_js_Project_2025_2
+2025년 2학년 Node.js 프로젝트 
--- SoyeonGameProject/Assets/Scripts/BuildingType.cs
+++
@@ -1,7 +0,0 @@
-public enum BuildingType
-{
-    Restaurant,             //음식집
-    Customer,              //고객 집
-    ChargingStation         //충전소
-}
-
